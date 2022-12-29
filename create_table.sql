e48dd60c362e86f403387b51da61c8dbc02f01a9
ANIK ACHARJEE <115971004+ANIKFSD@users.noreply.github.com>
2022-12-29 18:46:47 +0530

    Create Table Query

---
+++ create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Nurse (                                  
+    employeeid int,
+    Name varchar(255),
+    Position varchar(255),
+    Registered varchar(10),
+    ssn varchar(255)
+);
